0443ef975e2386d094f514bfa5dace9284f2a3dd
Matthew Brown <matthew.e.brown.17@gmail.com>
2026-02-25 21:17:33 -0500

    Streamline command-line args
    Do away with the subcommands for now; I just want to get this back into
    a robust, usable, working state for the simplest extraction case.

--- src/cli.rs
+++ src/cli.rs
@@ -1,65 +1,65 @@
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
-use clap::{Args, Parser, Subcommand, ValueEnum};
-use regex::Regex;
+use clap::ValueEnum;
 
-/// Unzip a 'gradebook' zip file from Blackboard into a neatly organized folder, with subfolders named after each
-/// student.
-#[derive(Parser, Debug)]
-#[command(author, version)]
-pub struct App {
-    /// The directory into which to extract student submissions.
-    ///
-    /// If one is not provided, a new directory will be created based on the name of the assignment.
-    #[arg(global = true)]
-    output_dir: Option<PathBuf>,
-
-    /// The naming scheme to use for student folders.
-    #[arg(short = 'n', long, value_name = "STYLE", default_value = "username", global = true, value_enum)]
-    name_style: NameStyle,
-
-    /// Skip extracting students' zip files.
-    ///
-    /// By default, any archive files submitted by students are automatically extracted into a folder with the same name
-    /// as the original file.
-    #[arg(short = 'X', long, global = true)]
-    no_extract: bool,
-
-    #[command(subcommand)]
-    command: Command,
-}
-
-#[derive(Subcommand, Debug)]
-pub enum Command {
-    /// Extract all submissions from a gradebook.
-    Extract(ExtractArgs),
-
-    /// Open a gradebook and display a list of submissions for manual handling.
-    Open(OpenArgs),
-}
-
-#[derive(Args, Debug)]
-pub struct CommonArgs {
+/// Unzip a "gradebook" zip file from Blackboard into a neatly organized folder, with subfolders named after each
+/// student/attempt.
+///
+/// By default, any `.zip`, `.tar`, `.tar.gz`, or `.rar` files in a submission will be automatically extracted into
+/// folders.
+#[derive(Debug, clap::Parser)]
+#[command(version)]
+pub struct Args {
     /// Path to a .zip file downloaded from the "Assignment File Download" tab in Blackboard's Grade Center.
     #[arg(name = "gradebook", value_name = "FILE")]
     path: PathBuf,
-}
 
-#[derive(Args, Debug)]
-pub struct OpenArgs {
-    #[command(flatten)]
-    common: CommonArgs,
-}
-
-#[derive(Args, Debug)]
-pub struct ExtractArgs {
-    #[command(flatten)]
-    common: CommonArgs,
-
-    /// Extract only students whose names match the given pattern.
+    /// The directory into which to extract student submissions.
     ///
-    /// Supported pattern syntax is given from the Rust `regex` crate: https://docs.rs/regex/latest/regex#syntax.
-    username_pattern: Option<Regex>, // Regex type supports FromStr out of the box!
+    /// If one is not provided, a new directory will be created based on the name of the assignment.
+    output_dir: Option<PathBuf>,
+
+    /// Disable automatic extraction of student-submitted archives altogether.
+    #[arg(long)]
+    no_unzip: bool,
+
+    /// Disable extraction of student-submitted archives larger than the given size.
+    ///
+    /// Sizes may be given with multiplicative unit suffixes. The following suffixes are supported:
+    /// - B=1,
+    /// - K=1024
+    /// - M=1048576
+    /// - G=1073741824
+    /// - KB=1000
+    /// - MB=1000000
+    /// - GB=1000000000
+    #[arg(long, value_name = "SIZE", value_parser = parse_filesize)]
+    unzip_max: Option<u64>,
+}
+
+#[allow(unused)]
+impl Args {
+    /// Parse a copy of [`Args`] from the command line, and exit on error.
+    pub fn parse() -> Self {
+        // This way we don't need to bring `clap::Parser` into scope elsewhere.
+        <Self as clap::Parser>::parse()
+    }
+
+    pub fn path(&self) -> &Path {
+        &self.path
+    }
+
+    pub fn output_dir(&self) -> Option<&Path> {
+        self.output_dir.as_deref()
+    }
+
+    pub fn no_unzip(&self) -> bool {
+        self.no_unzip
+    }
+
+    pub fn unzip_max(&self) -> Option<u64> {
+        self.unzip_max
+    }
 }
 
 #[derive(ValueEnum, Debug, Clone, Copy, PartialEq, Eq)]
@@ -70,4 +70,36 @@ pub enum NameStyle {
     FirstLast,
     /// Use students' full names in "Last, First" order (Smith, John).
     LastFirst,
+}
+
+#[derive(Debug, Clone, thiserror::Error)]
+enum FilesizeError {
+    #[error(transparent)]
+    InvalidInt(#[from] std::num::ParseIntError),
+    #[error("'{0}' is not a supported byte suffix")]
+    InvalidSuffix(String),
+}
+
+fn parse_filesize(s: &str) -> Result<u64, FilesizeError> {
+    let i = s
+        .char_indices()
+        .rfind(|(_, c)| c.is_ascii_digit()) // Find last digit before the suffix starts.
+        .map(|(i, _)| i + 1) // The position *after* the first digit is the start of the suffix.
+        .unwrap_or(s.len()); // If there is no suffix, the prefix goes all the way to the end.
+    let (num, suffix) = s.split_at(i);
+
+    let num = num.parse::<u64>()?;
+    let mult = match suffix {
+        "" => 1,
+        "B" | "b" => 1,
+        "K" | "k" => 1024,
+        "M" | "m" => 1024 * 1024,
+        "G" | "g" => 1024 * 1024 * 1024,
+        "KB" | "kb" => 1000,
+        "MB" | "mb" => 1000 * 1000,
+        "GB" | "gb" => 1000 * 1000 * 1000,
+        x => return Err(FilesizeError::InvalidSuffix(x.to_string())),
+    };
+
+    Ok(num * mult)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,27 @@
 mod cli;
 
+use std::error::Error;
+use std::fs::File;
+use std::io::{BufReader, Read, Seek};
+use std::path::Path;
 use std::process::ExitCode;
 
-use clap::Parser;
+use bb_gradebook::Gradebook;
 
 fn main() -> ExitCode {
-    let args = cli::App::parse();
+    let args = cli::Args::parse();
 
     println!("{:#?}", args);
 
+    let gradebook = open_gradebook(args.path()).unwrap();
+    println!("{:#?}", gradebook);
+
     ExitCode::SUCCESS
+}
+
+
+fn open_gradebook(path: &Path) -> Result<Gradebook<impl Read + Seek>, Box<dyn Error>> {
+    let file = BufReader::new(File::open(path)?);
+    let book = Gradebook::from_reader(file)?;
+    Ok(book)
 }
